8019d8020b958969c75a0babba01b82303773e8d
taku-enginner <taku.enginner@gmail.com>
2024-11-02 14:39:41 +0900

    ビューからモデルへデータ保存完了

--- app/controllers/ivents_controller.rb
+++ app/controllers/ivents_controller.rb
@@ -1,4 +1,18 @@
 class IventsController < ApplicationController
-  def home
+  def index
+  end
+
+  def new
+    @ivent = Ivent.new
+  end
+
+  def create
+    Ivent.create(ivent_params)
+    redirect_to root_path
+  end
+
+  private
+  def ivent_params
+    params.require(:ivent).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "ivents#home"
-  
+  resources :ivents
+  root "ivents#index"
 end
