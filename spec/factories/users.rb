d11a48ccaf8b390cebf0aee385b87d3667ba7711
taka4655 <taku.enginner@gmail.com>
2024-11-18 11:19:34 +0900

    fix:テストデータでFakerを使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email                 {"abe@gmail.com"}
+    email                 { Faker::Internet.unique.email } # ここをハードコーディングしてしまうと、GithubActionsで重複エラーが起こってしまう。たぶんActions内でデータを複数作るテストをしている。
     password              {"00000000"}
   end
 end
